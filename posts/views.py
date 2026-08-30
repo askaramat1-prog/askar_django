5fd573a3c90a2eec9b1b5771a309d1c3045e207c
askaramat1-prog <askaramat1@gmail.com>
2026-08-30 21:34:08 +0600

    django 4th mounth 1st hw

---
+++ posts/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http.response import HttpResponse
+# Create your views here.
+def hello_world(r):
+    return HttpResponse('<h1>hello world</h1>')
+
+def my_name(r):
+    name = 'askar'
+
+    return HttpResponse(f'<h2> Hello </h2> <h1>{name}</h1>')
+
+def say_name(r, name):
+    return HttpResponse(f'<h2> Hello </h2> <h1>{name}</h1>')
