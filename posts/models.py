1f10fbbbf0b12b39414b80fb21ebe770504df766
askaramat1-prog <askaramat1@gmail.com>
2026-09-03 16:36:38 +0600

    hw 2

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.CharField()
+    rate = models.IntegerField()
+    is_published = models.BooleanField(default=False)
