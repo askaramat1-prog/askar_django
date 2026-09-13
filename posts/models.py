0bdb46d31dee5d51b312fdffc7ea0811827c2dfc
askaramat1-prog <askaramat1@gmail.com>
2026-09-13 17:05:31 +0600

    hw3-4

--- posts/models.py
+++ posts/models.py
@@ -2,14 +2,14 @@ from django.db import models
 
 
 class Category(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=100)
 
     def __str__(self) -> str:
         return f"{self.name}"
 
 
 class Tag(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=100)
 
     def __str__(self) -> str:
         return f"{self.name}"
@@ -17,8 +17,15 @@ class Tag(models.Model):
 
 class Post(models.Model):
     title = models.CharField(max_length=255, unique=True)
-    text = models.CharField()
+    text = models.TextField()
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
     tags = models.ManyToManyField(Tag)
     image = models.ImageField(null=True, upload_to="posts")
     views = models.IntegerField(default=0)
+
+
+class Comment(models.Model):
+    authoer_name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    text = models.TextField()
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
